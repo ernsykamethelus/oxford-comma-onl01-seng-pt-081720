6a5193a6ae70526cf07a3e30c316e516952adbf2
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-26 01:04:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
-  if array.size.join == 1
+  if array.size == 1
     array.join (" ")
-  elsif array.size.join == 2
+  elsif array.size == 2
     array.join (" and ")
   else array.size >= 3
     long_array = array.pop
