a7ede7e88867719e0d1198e725787a729c338c22
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-26 00:54:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,3 +3,11 @@ def oxford_comma(array)
   array[0..1].join(" and ")
   array[0..1..3].join
 end
+
+#if my array has one element
+  #Do this
+#elsif has 2 elelemnts
+  #Do this
+#else
+  #Do this
+#end
